86d2d998d9b90254bb9dd2834392a3e233eae85c
richard <ziluoh@gmail.com>
2020-05-21 18:34:33 -0700

    Edit session form

--- frontend/src/components/session/signup_form.js
+++ frontend/src/components/session/signup_form.js
@@ -1,97 +1,100 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
+import './session.css';
 
 class SignupForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      username: '',
-      password: '',
-      password2: '',
-      errors: {}
-    };
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			username: '',
+			password: '',
+			password2: '',
+			errors: {},
+		};
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.clearedErrors = false;
-  }
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.clearedErrors = false;
+	}
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.signedIn === true) {
-      this.props.history.push('/login');
-    }
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.signedIn === true) {
+			this.props.history.push('/login');
+		}
 
-    this.setState({errors: nextProps.errors})
-  }
+		this.setState({ errors: nextProps.errors });
+	}
 
-  update(field) {
-    return e => this.setState({
-      [field]: e.currentTarget.value
-    });
-  }
+	update(field) {
+		return (e) =>
+			this.setState({
+				[field]: e.currentTarget.value,
+			});
+	}
 
-  handleSubmit(e) {
-    e.preventDefault();
-    let user = {
-      email: this.state.email,
-      username: this.state.username,
-      password: this.state.password,
-      password2: this.state.password2
-    };
+	handleSubmit(e) {
+		e.preventDefault();
+		let user = {
+			email: this.state.email,
+			username: this.state.username,
+			password: this.state.password,
+			password2: this.state.password2,
+		};
 
-    this.props.signup(user, this.props.history); 
-  }
+		this.props.signup(user, this.props.history);
+	}
 
-  renderErrors() {
-    return(
-      <ul>
-        {Object.keys(this.state.errors).map((error, i) => (
-          <li key={`error-${i}`}>
-            {this.state.errors[error]}
-          </li>
-        ))}
-      </ul>
-    );
-  }
+	renderErrors() {
+		return (
+			<ul>
+				{Object.keys(this.state.errors).map((error, i) => (
+					<li key={`error-${i}`}>{this.state.errors[error]}</li>
+				))}
+			</ul>
+		);
+	}
 
-  render() {
-    return (
-      <div className="session_form">
-        <form onSubmit={this.handleSubmit}>
-            <br/>
-              <input type="text"
-                value={this.state.email}
-                onChange={this.update('email')}
-                placeholder="Email"
-              />
-            <br/>
-              <input type="text"
-                value={this.state.username}
-                onChange={this.update('username')}
-                placeholder="Username"
-              />
-            <br/>
-              <input type="password"
-                value={this.state.password}
-                onChange={this.update('password')}
-                placeholder="Password"
-              />
-            <br/>
-              <input type="password"
-                value={this.state.password2}
-                onChange={this.update('password2')}
-                placeholder="Confirm Password"
-              />
-            <br/>
-            <input type="submit" value="Submit" />
-            {this.renderErrors()}
-            <p>
+	render() {
+		return (
+			<div className='session_form'>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type='email'
+						value={this.state.email}
+						onChange={this.update('email')}
+						placeholder='Email'
+						required='true'
+					/>
+					<input
+						type='text'
+						value={this.state.username}
+						onChange={this.update('username')}
+						placeholder='Username'
+						required='true'
+					/>
+					<input
+						type='password'
+						value={this.state.password}
+						onChange={this.update('password')}
+						placeholder='Password'
+						required='true'
+					/>
+					<input
+						type='password'
+						value={this.state.password2}
+						onChange={this.update('password2')}
+						placeholder='Confirm Password'
+						required='true'
+					/>
+					<input type='submit' value='Submit' />
+					{this.renderErrors()}
+					<p>
 						<Link to={'/login'}>Already have an account?</Link>
 					</p>
-        </form>
-      </div>
-    );
-  }
+				</form>
+			</div>
+		);
+	}
 }
 
 export default withRouter(SignupForm);
