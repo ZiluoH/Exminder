4f3dcfcbf6560345626e5fbd61f638749987f94b
Richard <ziluoh@gmail.com>
2020-05-27 22:28:55 -0700

    Updata navbar

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -3,38 +3,45 @@ import { Link } from 'react-router-dom';
 import './navbar.css';
 
 class NavBar extends React.Component {
-	constructor(props) {
-		super(props);
-		this.logoutUser = this.logoutUser.bind(this);
-		this.getLinks = this.getLinks.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.logoutUser = this.logoutUser.bind(this);
+    this.getLinks = this.getLinks.bind(this);
+  }
 
-	logoutUser(e) {
-		e.preventDefault();
-		this.props.logout();
-	}
+  logoutUser(e) {
+    e.preventDefault();
+    this.props.logout();
+  }
 
-	getLinks() {
-		if (this.props.loggedIn) {
-			return (
-				<div>
-					<Link to={'/tweets'}>All Tweets</Link>
-					<Link to={'/profile'}>Profile</Link>
-					<Link to={'/new_tweet'}>Write a Tweet</Link>
-					<button onClick={this.logoutUser}>Logout</button>
-				</div>
-			);
-		}
-	}
+  getLinks() {
+    if (this.props.loggedIn) {
+      return (
+        <ul className="nav-link">
+          <Link to={'/tweets'}>All Tweets</Link>
+          <Link to={'/profile'}>Profile</Link>
+          <Link to={'/new_tweet'}>Write a Tweet</Link>
+          <button onClick={this.logoutUser}>Logout</button>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="nav-link">
+          <Link to={'/login'}>Login</Link>
+          <Link to={'/signup'}>Signup</Link>
+        </ul>
+      );
+    }
+  }
 
-	render() {
-		return (
-			<div>
-				<header>Exminder</header>
-				{this.getLinks()}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <nav>
+        <header className='nav-header'>Exminder</header>
+        {this.getLinks()}
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
