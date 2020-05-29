c393d82ff3e1b8d4fc56515f50bb7bbae16942fb
richard <ziluoh@gmail.com>
2020-05-28 23:32:09 -0700

    Update landing page

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -3,45 +3,47 @@ import { Link } from 'react-router-dom';
 import './navbar.css';
 
 class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.logoutUser = this.logoutUser.bind(this);
-    this.getLinks = this.getLinks.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.logoutUser = this.logoutUser.bind(this);
+		this.getLinks = this.getLinks.bind(this);
+	}
 
-  logoutUser(e) {
-    e.preventDefault();
-    this.props.logout();
-  }
+	logoutUser(e) {
+		e.preventDefault();
+		this.props.logout();
+	}
 
-  getLinks() {
-    if (this.props.loggedIn) {
-      return (
-        <ul className="nav-link">
-          <Link to={'/tweets'}>All Tweets</Link>
-          <Link to={'/profile'}>Profile</Link>
-          <Link to={'/new_tweet'}>Write a Tweet</Link>
-          <button onClick={this.logoutUser}>Logout</button>
-        </ul>
-      );
-    } else {
-      return (
-        <ul className="nav-link">
-          <Link to={'/login'}>Login</Link>
-          <Link to={'/signup'}>Signup</Link>
-        </ul>
-      );
-    }
-  }
+	getLinks() {
+		if (this.props.loggedIn) {
+			return (
+				<ul className='nav-link'>
+					<Link to={'/tweets'}>All Tweets</Link>
+					<Link to={'/profile'}>Profile</Link>
+					<Link to={'/new_tweet'}>Write a Tweet</Link>
+					<button onClick={this.logoutUser}>Logout</button>
+				</ul>
+			);
+		} else {
+			return (
+				<ul className='nav-link'>
+					<Link to={'/login'}>Login</Link>
+					<Link to={'/signup'}>Signup</Link>
+				</ul>
+			);
+		}
+	}
 
-  render() {
-    return (
-      <nav>
-        <header className='nav-header'>Exminder</header>
-        {this.getLinks()}
-      </nav>
-    );
-  }
+	render() {
+		return (
+			<nav className='main-nav'>
+				<header className='nav-header'>
+					<span className='header-text'>Exminder</span>
+				</header>
+				{this.getLinks()}
+			</nav>
+		);
+	}
 }
 
 export default NavBar;
